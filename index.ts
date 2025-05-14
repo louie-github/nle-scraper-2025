71e4faa1e1f236ee3660cda095d818c2056fd9e6
Louie Torres <dev@lowkeytowers.com>
2025-05-14 22:00:11 +0800

    feat: mvp

--- index.ts
+++ index.ts
@@ -1,15 +1,29 @@
-import { Console, Effect, Match, pipe, Schedule } from "effect";
+import {
+  Array as EArray,
+  Console,
+  Effect,
+  Match,
+  pipe,
+  Schedule,
+} from "effect";
 import {
   getDataUrl,
-  type RegionData,
-  type ElectionReturnData,
-  GENERAL_DATA_URL,
-  type DataOrNull,
-  type Region,
+  getPrecinctUrl,
+  getErUrl,
+  type AreaData,
+  type ErData,
+  type GeoLevel,
+  LOCAL_START_CODE,
+  type Area,
+  GLRegion,
+  GLProvince,
+  GLCity,
+  GLBarangay,
 } from "./utils";
 import { promises as fs } from "node:fs";
 import path from "path";
 import type { UnknownException } from "effect/Cause";
+import { mkdir } from "node:fs/promises";
 
 class FileNotFoundError {
   readonly _tag = "FileNotFoundError";
@@ -19,7 +33,7 @@ class UnknownStatusCodeError {
   readonly _tag = "UnknownStatusCodeError";
 }
 
-const fetchRetryJsonData = (
+const fetchJson = (
   url: URL,
   policy: Schedule.Schedule<any, any, never> = Schedule.intersect(
     Schedule.recurs(5),
@@ -27,86 +41,142 @@ const fetchRetryJsonData = (
   ),
 ) =>
   pipe(
-    Effect.tryPromise(() => fetch(url))
+    Effect.tryPromise(() => fetch(url)),
     Effect.andThen((response) =>
       Match.value(response.status).pipe(
         Match.when(200, () => Effect.promise(() => response.json())),
-        Match.when(403, () => {
-          console.log(response);
-          return Effect.fail(new FileNotFoundError());
-        }),
+        // This also fails when Cloudflare blocks the request, but
+        // otherwise, this should represent a missing file.
+        Match.when(403, () => Effect.fail(new FileNotFoundError())),
         Match.orElse(() => Effect.fail(new UnknownStatusCodeError())),
       ),
     ),
     Effect.retry(policy),
   ) as Effect.Effect<
-    RegionData | ElectionReturnData,
+    AreaData | ErData,
     FileNotFoundError | UnknownStatusCodeError | UnknownException,
     never
   >;
 
-function saveData(
-  code: string,
-  data: RegionData | ElectionReturnData,
+function saveDataToFile(
+  filename: string,
+  data: any,
   folderPath: string,
 ) {
-  return Effect.tryPromise(() =>
-    fs.writeFile(
-      path.join(folderPath, `${code}.json`),
-      JSON.stringify(data),
-      "utf8",
-    ),
-  );
-}
-
-const fetchData = (code: string, url_prefix: URL = GENERAL_DATA_URL) =>
-  fetchRetryJsonData(getDataUrl(code, url_prefix));
-
-const fetchAndSaveData = (
-  code: string,
-  folderPath: string,
-  url_prefix: URL = GENERAL_DATA_URL,
-) =>
-  pipe(
-    fetchRetryJsonData(getDataUrl(code, url_prefix)),
-    Effect.tap((data) => saveData(code, data, folderPath)),
-  );
-
-function processRegion(
-  prefix: string,
-  mapFunc: (region: Region) => Effect.Effect<any, never, never>,
-) {
-  return (region: Region) =>
-    pipe(
-      Console.log(prefix + region.name),
-      Effect.andThen(() => fetchData(region.code)),
-      Effect.map((data) => data as RegionData),
-      Effect.andThen((data) =>
-        Effect.all(data.regions.map(mapFunc), { mode: "validate" }),
-      ),
-      Effect.catchAll(() => Console.log("Error encountered!")),
-    );
-}
-
-const program = pipe(
-  fetchData("0"),
-  Effect.orDie,
-  Effect.map((data) => data as RegionData),
-  Effect.andThen((data) =>
-    Effect.all(
-      data.regions.map(
-        processRegion(
-          "Region: ",
-          processRegion(
-            "  > Province: ",
-            processRegion("    > Municipality: ", () =>
-              Effect.succeed(null),
-            ),
-          ),
+  return pipe(
+    Effect.tryPromise(() => mkdir(folderPath, { recursive: true })),
+    Effect.andThen(() =>
+      Effect.tryPromise(() =>
+        fs.writeFile(
+          path.join(folderPath, `${filename}`),
+          JSON.stringify(data),
+          "utf8",
         ),
       ),
     ),
-  ),
-);
+  );
+}
+
+function fetchAreaData(code: string) {
+  return fetchJson(getDataUrl(code)).pipe(
+    Effect.catchAll(() => Effect.succeed(null)),
+  ) as Effect.Effect<AreaData | null, never, never>;
+}
+function fetchPrecinctData(code: string) {
+  return fetchJson(getPrecinctUrl(code)).pipe(
+    Effect.catchAll(() => Effect.succeed(null)),
+  ) as Effect.Effect<AreaData | null, never, never>;
+}
+function fetchErData(code: string) {
+  return fetchJson(getErUrl(code)).pipe(
+    Effect.catchAll(() => Effect.succeed(null)),
+  ) as Effect.Effect<ErData | null, never, never>;
+}
+
+function getRegionLevelFolder(
+  regionLevel: GeoLevel,
+  baseDirectory: string = "./data",
+): string {
+  if (regionLevel.parent) {
+    return path.join(
+      getRegionLevelFolder(regionLevel.parent),
+      regionLevel.name.replace("/", "-"),
+    );
+  } else {
+    return path.join(baseDirectory, regionLevel.name.replace("/", "-"));
+  }
+}
+
+function downloadData<T>(
+  area: Area,
+  level: GeoLevel,
+  pathTree: string[],
+  fetcher: (code: string) => Effect.Effect<T | null, never, never>,
+): Effect.Effect<T, never, never> {
+  const prefix = " ".repeat(level.level) + "> ";
+  return Effect.gen(function* () {
+    console.log(prefix + level.name);
+    const data = yield* fetcher(area.code);
+    if (data === null) {
+      console.log(prefix + "| Skipping, could not download JSON...");
+      yield* saveDataToFile(
+        `${area.code}.MISSING.json`,
+        {},
+        path.join(...pathTree),
+      );
+      // Hacky, but it works.
+      return { regions: [] };
+    }
+    yield* saveDataToFile(
+      `${area.code}.json`,
+      data,
+      path.join(...pathTree),
+    );
+    return data;
+  });
+}
+
+const DATA_DIRECTORY = path.join(".", "data");
+const program = Effect.gen(function* () {
+  const regions = yield* fetchAreaData(LOCAL_START_CODE);
+  if (regions === null) {
+    throw new Error("Failed to get initial region list!");
+  }
+  for (const region of regions.regions) {
+    const regionData = yield* fetchAreaData(region.code);
+    if (regionData === null) continue;
+
+    for (const province of regionData.regions) {
+      const provinceData = yield* fetchAreaData(province.code);
+      if (provinceData === null) continue;
+
+      for (const city of provinceData.regions) {
+        const cityData = yield* fetchAreaData(city.code);
+        if (cityData === null) continue;
+
+        for (const barangay of cityData.regions) {
+          const barangayData = yield* fetchPrecinctData(barangay.code);
+          if (barangayData === null) continue;
+
+          for (const precinct of barangayData.regions) {
+            const precinctData = yield* fetchErData(precinct.code);
+            if (precinctData === null) continue;
+
+            const filename = precinct.code.trim() + ".json";
+            const folderPath = path.join(
+              DATA_DIRECTORY,
+              region.name.replaceAll("/", "-").trim(),
+              province.name.replaceAll("/", "-").trim(),
+              city.name.replaceAll("/", "-").trim(),
+              barangay.name.replaceAll("/", "-").trim(),
+            );
+            console.log(`Saving: ${folderPath}/${filename}`);
+            yield* saveDataToFile(filename, precinctData, folderPath);
+          }
+        }
+      }
+    }
+  }
+});
 
 Effect.runPromise(program);
--- utils.ts
+++ utils.ts
@@ -1,6 +1,6 @@
 import { String as EString } from "effect";
 
-export const GENERAL_DATA_URL = new URL(
+export const AREA_DATA_URL = new URL(
   "https://2025electionresults.comelec.gov.ph/data/regions/local/",
 );
 export const PRECINCT_DATA_URL = new URL(
@@ -10,31 +10,93 @@ export const ER_DATA_URL = new URL(
   "https://2025electionresults.comelec.gov.ph/data/er/",
 );
 
-export interface Region {
+export const LOCAL_START_CODE = "0";
+
+export interface Area {
   categoryCode: string | null;
   masterCode: string | null;
   code: string;
   name: string;
 }
-export interface RegionData {
-  regions: Region[];
+export interface AreaData {
+  regions: Area[];
 }
-export type ElectionReturnData = {
-  _tag: "ElectionReturnData";
+
+export interface ErCandidate {
+  name: string;
+  votes: number;
+  percentage: number;
+}
+
+export interface ErContest {
+  contestCode: string;
+  contestName: string;
+  statistic: {
+    overVotes: number;
+    underVotes: number;
+    validVotes: number;
+    obtainedVotes: number;
+  };
+  candidates: {
+    candidates: ErCandidate[];
+  };
+}
+
+export interface ErData {
+  totalErReceived: number;
+  information: {
+    machineId: string;
+    location: string;
+    votingCenter: string;
+    precinctId: string;
+    precinctInCluster: string;
+    abstentions: number;
+    numberOfRegisteredVoters: number;
+    numberOfActuallyVoters: number;
+    numberOfValidBallot: number;
+    turnout: number;
+  };
+  national: ErContest[];
+  local: ErContest[];
+}
+
+export interface GeoLevel {
+  level: number;
+  name: string;
+  parent?: GeoLevel;
+}
+export const GLRegion: GeoLevel = { level: 0, name: "Region" };
+export const GLProvince: GeoLevel = {
+  level: 1,
+  name: "Province/District",
+  parent: GLRegion,
+};
+export const GLCity: GeoLevel = {
+  level: 2,
+  name: "City/Municipality",
+  parent: GLProvince,
+};
+export const GLBarangay: GeoLevel = {
+  level: 3,
+  name: "Barangay",
+  parent: GLCity,
 };
 
-export type DataOrNull = RegionData | ElectionReturnData | null;
+export type DataOrNull = AreaData | ErData | null;
 
-export function getDataUrl(code: string, url: URL = GENERAL_DATA_URL) {
+export function getDataUrl(code: string, url: URL = AREA_DATA_URL) {
   return new URL(`${code}.json`, url);
 }
 
-export function getPrecinctUrl(code: string) {
+export function getPrecinctUrl(
+  code: string,
+  url: URL = PRECINCT_DATA_URL,
+) {
   const prefix = EString.takeLeft(code, 2);
-  return new URL(`${prefix}/${code}.json`, PRECINCT_DATA_URL);
+  return new URL(`${prefix}/${code}.json`, url);
 }
 
-export function getErUrl(code: string) {
+export function getErUrl(code: string, url: URL = ER_DATA_URL) {
   const prefix = EString.takeLeft(code, 3);
-  return new URL(`${prefix}/${code}.json`, ER_DATA_URL);
+  return new URL(`${prefix}/${code}.json`, url);
 }
